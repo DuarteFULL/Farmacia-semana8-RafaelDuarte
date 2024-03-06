4d47a44bc0433a62a899b8469faa9cb8b26040ef
Rafael Duarte <rafael@prismarede.com.br>
2024-03-06 16:27:46 -0300

    Project Created

---
+++ farmacia8/src/main/java/br/com/farmacia/farmacia8/Farmacia8Application.java
@@ -0,0 +1,13 @@
+package br.com.farmacia.farmacia8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Farmacia8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Farmacia8Application.class, args);
+	}
+
+}
---
+++ farmacia8/src/test/java/br/com/farmacia/farmacia8/Farmacia8ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.farmacia.farmacia8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Farmacia8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
