fac877a35529ea894c3705bc7e8865acc26823b7
Rafael Duarte <rafael@prismarede.com.br>
2024-03-07 01:26:09 -0300

    Run

--- farmacia8/src/main/java/br/com/farmacia/farmacia8/Farmacia8Application.java
+++ farmacia8/src/main/java/br/com/farmacia/farmacia8/Farmacia8Application.java
@@ -1,20 +1,23 @@
 package br.com.farmacia.farmacia8;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 
 @SpringBootApplication
-public class Farmacia8Application  { //implements CommandLineRunner {
+@EntityScan(basePackages = "br.com.farmacia.farmacia8.modelo")
+public class Farmacia8Application  implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Farmacia8Application.class, args);
 	}
 
-	//@Override
-	//public void run(String... args) throws Exception {
-	//	System.out.println("Projeto Spring teste");
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Projeto Spring teste");
 		//throw new UnsupportedOperationException("Unimplemented method 'run'");
-	//}
+	}
 
 }
