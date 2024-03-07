917c08772c4edd2a766de3916035c91a0519a31c
Rafael Duarte <rafael@prismarede.com.br>
2024-03-07 02:03:33 -0300

    Menu and properties

--- farmacia8/src/main/java/br/com/farmacia/farmacia8/Farmacia8Application.java
+++ farmacia8/src/main/java/br/com/farmacia/farmacia8/Farmacia8Application.java
@@ -1,5 +1,7 @@
 package br.com.farmacia.farmacia8;
 
+import java.util.Scanner;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,10 +16,64 @@ public class Farmacia8Application  implements CommandLineRunner {
 		SpringApplication.run(Farmacia8Application.class, args);
 	}
 
+	private static Scanner teclado = new Scanner(System.in);
+
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Projeto Spring teste");
 		//throw new UnsupportedOperationException("Unimplemented method 'run'");
-	}
+		var opcao = Menu();
+
+        while (opcao != 8) {
+            try {
+                switch (opcao) {
+                    case 1:
+                        //listarProdutos();
+                        break;
+                    case 2:
+                        //cadastrarProduto();
+                        break;
+                    case 3:
+                        //deletarProduto();
+                        break;
+                    case 4:
+                        //consultarProduto();
+                        break;
+                    case 5:
+                        //alterarValorProduto();
+                        break;
+                    case 6:
+                        //alterarFabricanteProduto();
+                    case 7:
+                        //alterarDescricaoProduto();
+                }
+            } catch (Exception e){//RegraDenegocioException e) {
+                System.out.println("Erro: " +e.getMessage());
+                System.out.println("Pressione qualquer tecla e de ENTER para voltar ao menu");
+                teclado.next();
+            }
+
+            opcao = Menu();
+
+        }
+
+        System.out.println("Finalizando a aplicação.");
+	}   
+
+
+	private static int Menu() {
+        System.out.println("""
+                FARMÁCIA - ESCOLHA UMA OPÇÃO:
+                1 - Listar produtos
+                2 - Cadastrar produto
+                3 - Deletar produto
+                4 - Consultar produto
+                5 - Alterar valor do produto
+                6 - Alterar fabricante do produto 
+                7 - Alterar descrição do produto
+                8 - Sair
+                """);
+        return teclado.nextInt();
+    }
 
 }
--- farmacia8/src/main/java/br/com/farmacia/farmacia8/modelo/Fabricante.java
+++ farmacia8/src/main/java/br/com/farmacia/farmacia8/modelo/Fabricante.java
@@ -1,10 +1,16 @@
 package br.com.farmacia.farmacia8.modelo;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+// import javax.persistence.Entity;
+// import javax.persistence.GeneratedValue;
+// import javax.persistence.GenerationType;
+// import javax.persistence.Id;
+// import javax.persistence.Table;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "fabricantes")
